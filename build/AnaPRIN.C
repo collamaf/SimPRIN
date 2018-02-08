6cad21cee31aa96163d0c6be5da8a4a5981ff8a6
FRANCESCO COLLAMATI <francesco.collamati@roma1.infn.it>
2018-02-08 17:47:26 +0100

    AnaPRIN working but before restyling

--- build/AnaPRIN.C
+++ build/AnaPRIN.C
@@ -5,9 +5,22 @@
 #include <TCanvas.h>
 #include <map>
 
-#define NBIN 1000
-#define MIN 0
-#define MAX 30
+#define NBINE 1000
+#define MINE 0
+#define MAXE 20
+
+#define NBINX 60
+#define MINX -3
+#define MAXX 3
+
+#define NBINY 60
+#define MINY -3
+#define MAXY 3
+
+#define NBINZ 120
+#define MINZ -10
+#define MAXZ 110
+
 
 void AnaPRIN::Loop()
 {
@@ -26,7 +39,13 @@ void AnaPRIN::Loop()
 	
 	Long64_t nentries = fChain->GetEntriesFast();
 	
-	TH1F* TuttiGamma=new TH1F("All","All", NBIN, MIN, MAX);
+	TH1F* TuttiGammaEne=new TH1F("AllEne","AllEne", NBINE, MINE, MAXE);
+	TH1F* TuttiGammaX=new TH1F("AllX","AllX", NBINX, MINX, MAXX);
+	TH1F* TuttiGammaY=new TH1F("AllY","AllY", NBINY, MINY, MAXY);
+	TH1F* TuttiGammaZ=new TH1F("AllZ","AllZ", NBINZ, MINZ, MAXZ);
+
+	THStack *StackEne = new THStack("StackEne","");
+	THStack *StackZ = new THStack("StackZ","");
 
 	int temp=-10, jj, debug=0, tt=0;
 	vector<int>     AllMothers;
@@ -43,7 +62,10 @@ void AnaPRIN::Loop()
 		temp=0;
 		
 		for (jj=0; jj<ExitGammasEne->size();jj++) { //Loop su tutti i fotoni uscenti dal cilindro in quell'evento
-			TuttiGamma->Fill(ExitGammasEne->at(jj));
+			TuttiGammaEne->Fill(ExitGammasEne->at(jj));
+			TuttiGammaX->Fill(AllX->at(jj));
+			TuttiGammaY->Fill(AllY->at(jj));
+			TuttiGammaZ->Fill(AllZ->at(jj));
 
 			if (debug) cout<<"Ev "<< jentry<<", jj "<< jj<<", temp "<< temp<<", Mother "<<ExitGammasMother->at(jj)<<endl;
 
@@ -69,11 +91,17 @@ void AnaPRIN::Loop()
 	// Finito il primo loop ho riempito un vettore (AllMothers) con tutti gli isotopi trovati e una mappa (mappa) con la loro posizione
 	
 	// Creo l'array di istogrammi per i vari isotopi
-	TH1F* Histo[AllMothers.size()];
+	TH1F* HistoEne[AllMothers.size()];
+	TH1F* HistoZ[AllMothers.size()];
 	cout<<"Numero elementi "<<AllMothers.size()<<endl;
 	for (jj=0; jj<AllMothers.size(); jj++) {
-		Histo[jj]=new TH1F(Form("Histo%d",AllMothers.at(jj)%100000),Form("Histo%d",AllMothers.at(jj)%100000), NBIN, MIN, MAX);
-		cout<<"Creato istogramma num= "<< jj<<", nome= "<<Form("Histo%d",AllMothers.at(jj)%100000)<<endl;
+		HistoEne[jj]=new TH1F(Form("HistoEne%d",AllMothers.at(jj)%100000),Form("HistoEne%d",AllMothers.at(jj)%100000), NBINE, MINE, MAXE);
+		cout<<"Creato istogramma Ene num= "<< jj<<", nome= "<<Form("HistoEne%d",AllMothers.at(jj)%100000)<<endl;
+		HistoEne[jj]->SetLineColor(kBlack);
+
+		HistoZ[jj]=new TH1F(Form("HistoZ%d",AllMothers.at(jj)%100000),Form("HistoZ%d",AllMothers.at(jj)%100000), NBINZ, MINZ, MAXZ);
+		cout<<"Creato istogramma Z num= "<< jj<<", nome= "<<Form("HistoEne%d",AllMothers.at(jj)%100000)<<endl;
+		
 	}
 	
 	for (Long64_t jentry=0; jentry<nentries;jentry++) { // SECONDO LOOP SU TUTTI GLI EVENTI: Riscorro tutti gli eventi per riempire gli istogrammi dei vari elementi
@@ -92,8 +120,9 @@ void AnaPRIN::Loop()
 				cout<<"DEBUG ERRORE! Isotopo non trovato"<<endl; //non dovrebbe succedere...
 			}
 			
-			Histo[iter->second]->Fill(ExitGammasEne->at(jj)); //Riempio l'istogramma corrispondente
-			
+			HistoEne[iter->second]->Fill(ExitGammasEne->at(jj)); //Riempio l'istogramma di E corrispondente
+			HistoZ[iter->second]->Fill(AllZ->at(jj)); //Riempio l'istogramma di Z corrispondente
+
 			
 		}
 		
@@ -102,20 +131,77 @@ void AnaPRIN::Loop()
 	if (debug) cout<<"DEBUG Carbonio12 sta alla posizione "<<mappa[1000060120]<<endl;
 	
 
-	TuttiGamma->SetLineColor(kBlack);
-	TuttiGamma->Draw();
+	TuttiGammaEne->SetLineColor(kBlack);
+	TuttiGammaEne->Draw();
+	
+	TuttiGammaEne->Write();
+	TuttiGammaX->Write();
+	TuttiGammaY->Write();
+	TuttiGammaZ->Write();
 
+	auto C11 = mappa.find(1000060110);
+	cout<<"Trovato C11 in posizone: "<<C11->second<<endl;
+	auto C12 = mappa.find(1000060120);
+	cout<<"Trovato C12 in posizone: "<<C12->second<<endl;
+	auto C13 = mappa.find(1000060130);
+	cout<<"Trovato C13 in posizone: "<<C13->second<<endl;
+	auto O15 = mappa.find(1000080150);
+	cout<<"Trovato O15 in posizone: "<<O15->second<<endl;
+	auto O16 = mappa.find(1000080160);
+	cout<<"Trovato O16 in posizone: "<<O16->second<<endl;
+	auto F17 = mappa.find(1000090170);
+	cout<<"Trovato F17 in posizone: "<<F17->second<<endl;
+	auto He4 = mappa.find(1000020040);
+	cout<<"Trovato He4 in posizone: "<<He4->second<<endl;
+	auto N14 = mappa.find(1000070140);
+	cout<<"Trovato N14 in posizone: "<<N14->second<<endl;
+	auto N15 = mappa.find(1000070150);
+	cout<<"Trovato N15 in posizone: "<<N15->second<<endl;
+	auto B10 = mappa.find(1000050100);
+	cout<<"Trovato N15 in posizone: "<<B10->second<<endl;
+	auto B11 = mappa.find(1000050110);
+	cout<<"Trovato B11 in posizone: "<<B11->second<<endl;
 	
-	TuttiGamma->Write();
 	
+	HistoEne[C11->second]->SetFillColor(kRed); //C11
+	HistoEne[C12->second]->SetFillColor(kBlue); //C12
+	HistoEne[C13->second]->SetFillColor(kCyan+2); //C13
+	HistoEne[O15->second]->SetFillColor(kMagenta); //O15
+	HistoEne[O16->second]->SetFillColor(kGreen); //O16
+	HistoEne[F17->second]->SetFillColor(kOrange); //F17
+	HistoEne[He4->second]->SetFillColor(kCyan); //He4
+	HistoEne[N14->second]->SetFillColor(kYellow); //N14
+	HistoEne[N15->second]->SetFillColor(kViolet+6); //N15
+	HistoEne[B10->second]->SetFillColor(kGray+1); //B10
+	HistoEne[B11->second]->SetFillColor(kOrange-6); //B11
 	
 	for (jj=0; jj<AllMothers.size(); jj++) {
 //		cout<<AllMothers.at(jj)<<endl;
-		Histo[jj]->Write();
-		
+		HistoEne[jj]->Write();
+		HistoZ[jj]->Write();
+		StackEne->Add(HistoEne[jj]);
+		StackZ->Add(HistoZ[jj]);
+
 	}
 	
+//	StackEne[jj].Write();
+//	StackZ[jj].Write();
 	
+	TCanvas* canv1=new TCanvas();
+	TuttiGammaX->Draw();
+	
+	TCanvas* canv2=new TCanvas();
+	TuttiGammaY->Draw();
+	
+	TCanvas* canv3=new TCanvas();
+	TuttiGammaZ->Draw();
+	
+	TCanvas* canvasStackEne=new TCanvas();
+	StackEne->Draw();
+	
+	TLegend* legendEne=new TLegend();
+	legendEne->AddEntry(HistoEne[C11->second],HistoEne[C11->second]->GetTitle());
+	legendEne->Draw();
 	
 }
 
--- build/AnaPRIN.h
+++ build/AnaPRIN.h
@@ -17,73 +17,73 @@
 #include "vector"
 
 class AnaPRIN {
-public :
-   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
-   Int_t           fCurrent; //!current Tree number in a TChain
-
-// Fixed size dimensions of array or collections stored in the TTree if any.
-
-   // Declaration of leaf types
-   Double_t        AllX;
-   Double_t        AllY;
-   Double_t        AllZ;
-   vector<double>  *AllCosX;
-   vector<double>  *AllCosY;
-   vector<double>  *AllCosZ;
-   vector<double>  *AllEne;
-   vector<double>  *AllIsotope;
-   vector<double>  *ExitX;
-   vector<double>  *ExitY;
-   vector<double>  *ExitZ;
-   vector<double>  *ExitCosX;
-   vector<double>  *ExitCosY;
-   vector<double>  *ExitCosZ;
-   vector<double>  *ExitEne;
-   vector<double>  *ExitPart;
-   vector<double>  *ExitParentID;
-   vector<int>     *ExitProcess;
-   Double_t        ExitTrackN;
-   vector<double>  *ExitGammasEne;
-   vector<int>     *ExitGammasMother;
-
-
-
+	public :
+	TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+	Int_t           fCurrent; //!current Tree number in a TChain
 	
-   // List of branches
-   TBranch        *b_AllX;   //!
-   TBranch        *b_AllY;   //!
-   TBranch        *b_AllZ;   //!
-   TBranch        *b_AllCosX;   //!
-   TBranch        *b_AllCosY;   //!
-   TBranch        *b_AllCosZ;   //!
-   TBranch        *b_AllEne;   //!
-   TBranch        *b_AllIsotope;   //!
-   TBranch        *b_ExitX;   //!
-   TBranch        *b_ExitY;   //!
-   TBranch        *b_ExitZ;   //!
-   TBranch        *b_ExitCosX;   //!
-   TBranch        *b_ExitCosY;   //!
-   TBranch        *b_ExitCosZ;   //!
-   TBranch        *b_ExitEne;   //!
-   TBranch        *b_ExitPart;   //!
-   TBranch        *b_ExitParentID;   //!
-   TBranch        *b_ExitProcess;   //!
-   TBranch        *b_ExitTrackN;   //!
-   TBranch        *b_ExitGammasEne;   //!
-   TBranch        *b_ExitGammasMother;   //!
-
-//   AnaPRIN(TTree *tree=0);
+	// Fixed size dimensions of array or collections stored in the TTree if any.
+	
+	// Declaration of leaf types
+	vector<double>  *AllX;
+	vector<double>  *AllY;
+	vector<double>  *AllZ;
+	vector<double>  *AllCosX;
+	vector<double>  *AllCosY;
+	vector<double>  *AllCosZ;
+	vector<double>  *AllEne;
+	vector<double>  *AllIsotope;
+	vector<double>  *ExitX;
+	vector<double>  *ExitY;
+	vector<double>  *ExitZ;
+	vector<double>  *ExitCosX;
+	vector<double>  *ExitCosY;
+	vector<double>  *ExitCosZ;
+	vector<double>  *ExitEne;
+	vector<double>  *ExitPart;
+	vector<double>  *ExitParentID;
+	vector<int>     *ExitProcess;
+	Double_t        ExitTrackN;
+	vector<double>  *ExitGammasEne;
+	vector<int>     *ExitGammasMother;
+	
+	
+	
+	
+	// List of branches
+	TBranch        *b_AllX;   //!
+	TBranch        *b_AllY;   //!
+	TBranch        *b_AllZ;   //!
+	TBranch        *b_AllCosX;   //!
+	TBranch        *b_AllCosY;   //!
+	TBranch        *b_AllCosZ;   //!
+	TBranch        *b_AllEne;   //!
+	TBranch        *b_AllIsotope;   //!
+	TBranch        *b_ExitX;   //!
+	TBranch        *b_ExitY;   //!
+	TBranch        *b_ExitZ;   //!
+	TBranch        *b_ExitCosX;   //!
+	TBranch        *b_ExitCosY;   //!
+	TBranch        *b_ExitCosZ;   //!
+	TBranch        *b_ExitEne;   //!
+	TBranch        *b_ExitPart;   //!
+	TBranch        *b_ExitParentID;   //!
+	TBranch        *b_ExitProcess;   //!
+	TBranch        *b_ExitTrackN;   //!
+	TBranch        *b_ExitGammasEne;   //!
+	TBranch        *b_ExitGammasMother;   //!
+	
+	//   AnaPRIN(TTree *tree=0);
 	AnaPRIN(TString filename);
 	TString nomefile;
-
-   virtual ~AnaPRIN();
-   virtual Int_t    Cut(Long64_t entry);
-   virtual Int_t    GetEntry(Long64_t entry);
-   virtual Long64_t LoadTree(Long64_t entry);
-   virtual void     Init(TTree *tree);
-   virtual void     Loop();
-   virtual Bool_t   Notify();
-   virtual void     Show(Long64_t entry = -1);
+	
+	virtual ~AnaPRIN();
+	virtual Int_t    Cut(Long64_t entry);
+	virtual Int_t    GetEntry(Long64_t entry);
+	virtual Long64_t LoadTree(Long64_t entry);
+	virtual void     Init(TTree *tree);
+	virtual void     Loop();
+	virtual Bool_t   Notify();
+	virtual void     Show(Long64_t entry = -1);
 	TFile* outfile;
 };
 
@@ -106,110 +106,113 @@ AnaPRIN::AnaPRIN(TString filename)
 
 AnaPRIN::~AnaPRIN()
 {
-   if (!fChain) return;
-   delete fChain->GetCurrentFile();
+	if (!fChain) return;
+	delete fChain->GetCurrentFile();
 }
 
 Int_t AnaPRIN::GetEntry(Long64_t entry)
 {
-// Read contents of entry.
-   if (!fChain) return 0;
-   return fChain->GetEntry(entry);
+	// Read contents of entry.
+	if (!fChain) return 0;
+	return fChain->GetEntry(entry);
 }
 Long64_t AnaPRIN::LoadTree(Long64_t entry)
 {
-// Set the environment to read one entry
-   if (!fChain) return -5;
-   Long64_t centry = fChain->LoadTree(entry);
-   if (centry < 0) return centry;
-   if (fChain->GetTreeNumber() != fCurrent) {
-      fCurrent = fChain->GetTreeNumber();
-      Notify();
-   }
-   return centry;
+	// Set the environment to read one entry
+	if (!fChain) return -5;
+	Long64_t centry = fChain->LoadTree(entry);
+	if (centry < 0) return centry;
+	if (fChain->GetTreeNumber() != fCurrent) {
+		fCurrent = fChain->GetTreeNumber();
+		Notify();
+	}
+	return centry;
 }
 
 void AnaPRIN::Init(TTree *tree)
 {
-   // The Init() function is called when the selector needs to initialize
-   // a new tree or chain. Typically here the branch addresses and branch
-   // pointers of the tree will be set.
-   // It is normally not necessary to make changes to the generated
-   // code, but the routine can be extended by the user if needed.
-   // Init() will be called many times when running on PROOF
-   // (once per file to be processed).
-
-   // Set object pointer
-   AllCosX = 0;
-   AllCosY = 0;
-   AllCosZ = 0;
-   AllEne = 0;
-   AllIsotope = 0;
-   ExitX = 0;
-   ExitY = 0;
-   ExitZ = 0;
-   ExitCosX = 0;
-   ExitCosY = 0;
-   ExitCosZ = 0;
-   ExitEne = 0;
-   ExitPart = 0;
-   ExitParentID = 0;
-   ExitProcess = 0;
-   ExitGammasEne = 0;
-   ExitGammasMother = 0;
-   // Set branch addresses and branch pointers
-   if (!tree) return;
-   fChain = tree;
-   fCurrent = -1;
-   fChain->SetMakeClass(1);
-
-   fChain->SetBranchAddress("AllX", &AllX, &b_AllX);
-   fChain->SetBranchAddress("AllY", &AllY, &b_AllY);
-   fChain->SetBranchAddress("AllZ", &AllZ, &b_AllZ);
-   fChain->SetBranchAddress("AllCosX", &AllCosX, &b_AllCosX);
-   fChain->SetBranchAddress("AllCosY", &AllCosY, &b_AllCosY);
-   fChain->SetBranchAddress("AllCosZ", &AllCosZ, &b_AllCosZ);
-   fChain->SetBranchAddress("AllEne", &AllEne, &b_AllEne);
-   fChain->SetBranchAddress("AllIsotope", &AllIsotope, &b_AllIsotope);
-   fChain->SetBranchAddress("ExitX", &ExitX, &b_ExitX);
-   fChain->SetBranchAddress("ExitY", &ExitY, &b_ExitY);
-   fChain->SetBranchAddress("ExitZ", &ExitZ, &b_ExitZ);
-   fChain->SetBranchAddress("ExitCosX", &ExitCosX, &b_ExitCosX);
-   fChain->SetBranchAddress("ExitCosY", &ExitCosY, &b_ExitCosY);
-   fChain->SetBranchAddress("ExitCosZ", &ExitCosZ, &b_ExitCosZ);
-   fChain->SetBranchAddress("ExitEne", &ExitEne, &b_ExitEne);
-   fChain->SetBranchAddress("ExitPart", &ExitPart, &b_ExitPart);
-   fChain->SetBranchAddress("ExitParentID", &ExitParentID, &b_ExitParentID);
-   fChain->SetBranchAddress("ExitProcess", &ExitProcess, &b_ExitProcess);
-   fChain->SetBranchAddress("ExitTrackN", &ExitTrackN, &b_ExitTrackN);
-   fChain->SetBranchAddress("ExitGammasEne", &ExitGammasEne, &b_ExitGammasEne);
-   fChain->SetBranchAddress("ExitGammasMother", &ExitGammasMother, &b_ExitGammasMother);
-   Notify();
+	// The Init() function is called when the selector needs to initialize
+	// a new tree or chain. Typically here the branch addresses and branch
+	// pointers of the tree will be set.
+	// It is normally not necessary to make changes to the generated
+	// code, but the routine can be extended by the user if needed.
+	// Init() will be called many times when running on PROOF
+	// (once per file to be processed).
+	
+	// Set object pointer
+	AllX = 0;
+	AllY = 0;
+	AllZ = 0;
+	AllCosX = 0;
+	AllCosY = 0;
+	AllCosZ = 0;
+	AllEne = 0;
+	AllIsotope = 0;
+	ExitX = 0;
+	ExitY = 0;
+	ExitZ = 0;
+	ExitCosX = 0;
+	ExitCosY = 0;
+	ExitCosZ = 0;
+	ExitEne = 0;
+	ExitPart = 0;
+	ExitParentID = 0;
+	ExitProcess = 0;
+	ExitGammasEne = 0;
+	ExitGammasMother = 0;
+	// Set branch addresses and branch pointers
+	if (!tree) return;
+	fChain = tree;
+	fCurrent = -1;
+	fChain->SetMakeClass(1);
+	
+	fChain->SetBranchAddress("AllX", &AllX, &b_AllX);
+	fChain->SetBranchAddress("AllY", &AllY, &b_AllY);
+	fChain->SetBranchAddress("AllZ", &AllZ, &b_AllZ);
+	fChain->SetBranchAddress("AllCosX", &AllCosX, &b_AllCosX);
+	fChain->SetBranchAddress("AllCosY", &AllCosY, &b_AllCosY);
+	fChain->SetBranchAddress("AllCosZ", &AllCosZ, &b_AllCosZ);
+	fChain->SetBranchAddress("AllEne", &AllEne, &b_AllEne);
+	fChain->SetBranchAddress("AllIsotope", &AllIsotope, &b_AllIsotope);
+	fChain->SetBranchAddress("ExitX", &ExitX, &b_ExitX);
+	fChain->SetBranchAddress("ExitY", &ExitY, &b_ExitY);
+	fChain->SetBranchAddress("ExitZ", &ExitZ, &b_ExitZ);
+	fChain->SetBranchAddress("ExitCosX", &ExitCosX, &b_ExitCosX);
+	fChain->SetBranchAddress("ExitCosY", &ExitCosY, &b_ExitCosY);
+	fChain->SetBranchAddress("ExitCosZ", &ExitCosZ, &b_ExitCosZ);
+	fChain->SetBranchAddress("ExitEne", &ExitEne, &b_ExitEne);
+	fChain->SetBranchAddress("ExitPart", &ExitPart, &b_ExitPart);
+	fChain->SetBranchAddress("ExitParentID", &ExitParentID, &b_ExitParentID);
+	fChain->SetBranchAddress("ExitProcess", &ExitProcess, &b_ExitProcess);
+	fChain->SetBranchAddress("ExitTrackN", &ExitTrackN, &b_ExitTrackN);
+	fChain->SetBranchAddress("ExitGammasEne", &ExitGammasEne, &b_ExitGammasEne);
+	fChain->SetBranchAddress("ExitGammasMother", &ExitGammasMother, &b_ExitGammasMother);
+	Notify();
 }
 
 Bool_t AnaPRIN::Notify()
 {
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
-
-   return kTRUE;
+	// The Notify() function is called when a new file is opened. This
+	// can be either for a new TTree in a TChain or when when a new TTree
+	// is started when using PROOF. It is normally not necessary to make changes
+	// to the generated code, but the routine can be extended by the
+	// user if needed. The return value is currently not used.
+	
+	return kTRUE;
 }
 
 void AnaPRIN::Show(Long64_t entry)
 {
-// Print contents of entry.
-// If entry is not specified, print current entry
-   if (!fChain) return;
-   fChain->Show(entry);
+	// Print contents of entry.
+	// If entry is not specified, print current entry
+	if (!fChain) return;
+	fChain->Show(entry);
 }
 Int_t AnaPRIN::Cut(Long64_t entry)
 {
-// This function may be called from Loop.
-// returns  1 if entry is accepted.
-// returns -1 otherwise.
-   return 1;
+	// This function may be called from Loop.
+	// returns  1 if entry is accepted.
+	// returns -1 otherwise.
+	return 1;
 }
 #endif // #ifdef AnaPRIN_cxx
